73374fa18739649746ebaea5c6c675e8242f0c35
Michael Prather <mprather1@gmail.com>
2016-06-06 21:03:07 -0500

    PostListView

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
+_.templateSettings = {
+  interpolate: /\{\{(.+?)\}\}/g
+};
+
 var Post = Backbone.Model.extend({});
 var Posts = Backbone.Collection.extend({
   model: Post,
   url: "/posts"
+});
+
+var PostListView = Backbone.View.extend({
+  tagName: "li",
+  template: _.template("a href='/posts/{{id}}'>{{title}}</a>"),
+  render: function(){
+    this.el.innerHTML = this.template(this.model.toJSON());
+    return this;
+  }
 });
